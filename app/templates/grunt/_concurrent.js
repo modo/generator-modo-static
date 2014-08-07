536a2c7f3945376bd8064a2eb93240366e313625
Josh Lasdin <josh@mododesigngroup.com>
2014-08-07 09:12:15 -0400

    Added javadoc description to concurrent config

--- app/templates/grunt/_concurrent.js
+++ app/templates/grunt/_concurrent.js
@@ -1,3 +1,6 @@
+/**
+ * Performs multiple tasks at once for speedier builds
+ * @see https://github.com/sindresorhus/grunt-concurrent
 module.exports = function (grunt, options) {
     return {
         options: {
